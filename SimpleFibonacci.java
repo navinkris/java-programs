c2a926925c291b50f30947d3666133d7712ed4df
Navin Krishnamurthi <navinkrishnamurthi9994@gmail.com>
2023-08-30 20:41:43 +0530

    SimpleFibonacci.java
    Program to compute Fibonacci series of n numbers in Java

---
+++ SimpleFibonacci.java
@@ -0,0 +1,21 @@
+// Program to compute Fibonacci series of n numbers in Java
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		int firstNum=0, secondNum=1, res, num;
+		Scanner s1 = new Scanner(System.in);
+		num = s1.nextInt();
+		for(int i=1; i<=num; i++) {
+			if(i != num) {
+					System.out.print(firstNum+ " ");
+			} else {
+					System.out.print(firstNum);
+			}
+			res = firstNum + secondNum;
+			firstNum = secondNum;
+			secondNum = res;
+		}
+	}
+}
